1861c9091d290761056e0ab1ee8f190cbd4304db
Abhishek Raj <abhraj1@Abhisheks-MacBook-Pro.local>
2022-06-01 03:22:14 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const JS_URL =
+  "https://raw.githubusercontent.com/abhishekraj007/md2json/main/js-v2.json";
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(JS_URL);
+        const data = await res.json();
+        setData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Question</h1>
+      {(data ?? []).map((question) => {
+        return (
+          <div key={question.title}>
+            <h3>{question.title}</h3>
+
+            {question.content.map((ele, index) => {
+              return (
+                <div key={index} dangerouslySetInnerHTML={{ __html: ele }} />
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
